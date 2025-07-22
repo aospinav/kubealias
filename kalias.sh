374e38666e9ca2a018be46fb403873409336d45f
aospinav <110316074+aospinav@users.noreply.github.com>
2025-07-22 11:42:55 -0500

    Create kalias.sh

---
+++ kalias.sh
@@ -0,0 +1,9 @@
+do='--dry-run=client -o yaml'
+
+# Alias
+alias k='kubectl'
+alias kg='kubectl get'
+alias kd='kubectl describe'
+alias ka='kubectl apply -f'
+alias kl='kubectl logs'
+alias kctx='kubectl config use-context'
